83713f84b86252e152800f5cd106acfe9766af0d
Laura Lee <lauratlee48@gmail.com>
2025-06-11 14:11:31 -0400

    Update cross_class_alignments.py

--- scripts/cross_class_alignments.py
+++ scripts/cross_class_alignments.py
@@ -5,3 +5,11 @@
 # example usage (run from thyme_lab_internship_2025/)
 # python scripts/cross_class_alignments.py 2.0
 
+# a temporary dictionary is created where the key is a tuple of (human gene, zebrafish gene), and the value is a list of tuples with the corresponding alignment data.
+  # value tuple format is (match, mismatch, % similarity, class, method). The classes are A, B1, C, or F, or "A (4s0V)" (referring to the initial alignments done with 4s0v, a class A GPCR, as the reference)
+
+import os, sys
+
+
+
+
