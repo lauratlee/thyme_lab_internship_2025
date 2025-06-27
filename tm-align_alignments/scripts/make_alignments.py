1c0ddbacb36b7ceddcdd9b79ee45f5228b6a48cf
Laura Lee <lauratlee48@gmail.com>
2025-06-27 17:04:29 -0400

    Update make_alignments.py

--- tm-align_alignments/scripts/make_alignments.py
+++ tm-align_alignments/scripts/make_alignments.py
@@ -41,7 +41,7 @@ def tm_align_runner(gpcr_dir):
 
             print(f"NAME: {name}")
 
-            os.system(f"~/TMalign '{gene}' '{sys.argv[1]}' -o '~/thyme_lab_internship_2025/tm-align_alignments/gpcr_pocket_dir/{gpcr_dir}/{gpcr_class}/{name}'")
+            os.system(f"~/TMalign '{gene}' '{sys.argv[1]}' -o ~/thyme_lab_internship_2025/tm-align_alignments/gpcr_pocket_dir/{gpcr_dir}/{gpcr_class}/{name}.pdb")
 
             print(f"saved ... {gene} complete")
 
