62132e2db7ab85196afba6985c189a36029ed68a
Laura Lee <lauratlee48@gmail.com>
2025-07-28 12:11:03 -0400

    Create run_bsubs.py

---
+++ alphafold_benchmarking/scripts/run_bsubs.py
@@ -0,0 +1,18 @@
+# run from specific system folder, e.g. 9HZ0/
+
+import os, sys
+
+base = os.path.basename(".")
+
+for dir in os.listdir("."):
+  if dir.startswith("res_"):
+    num = dir.split("_")[1]
+    os.chdir(dir)
+    job_name = f"{base}_{num}.slurm"
+    if not os.path.exists(job_name):
+      print("[WARNING] file does not exist")
+      sys.exit(1)
+    else:
+      os.system(f"bsub {job_name}")
+    os.chdir("..")
+  
