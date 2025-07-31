511ac58389191b0f83b081001b05f9c54c8362a8
Laura Lee <lauratlee48@gmail.com>
2025-07-31 17:22:31 -0400

    Update organize_placements.py

--- alphafold_benchmarking/scripts/organize_placements.py
+++ alphafold_benchmarking/scripts/organize_placements.py
@@ -4,31 +4,48 @@
 import os, math
 
 for r1,d1,f1 in os.walk(os.getcwd()):
-  for dir in d1:
+  for dir1 in d1:
     #filter for residue placement folders
-    if "res_" in dir:
-      print(dir)
-      os.chdir(dir)
+    if "res_" in dir1:
+      os.chdir(dir1)
   
       #iterate thru files in residue folder to count total number of placements
       count = 0
       for r2,d2,f2 in os.walk(os.getcwd()):
-        for file in f2:
-          if ".pdb" in file:
+        for file2 in f2:
+          if ".pdb" in file2:
             count += 1
   
-      print(f"{count} placements in {dir}")
+      print(f"{count} placements in {dir1}")
 
+      #calculate total # of groups of 1000
       groups = math.ceil(count / 1000)
-      print(f"{groups} groups")
+
+      #create directories to sort placements into
+      for i in range(0, groups):
+        os.system(f"mkdir {i}")
+
+      #iterate through files again to sort them into their respective folders
+      for r3,d3,f3 in os.walk(os.getcwd()):
+        for file3 in f3:
+          if ".pdb" not in file3:
+            continue
+
+          #split a file name like "chain_A_ligand_0.pdb" to extract "0.pdb", then split again to extract the placement index (in this case 0)
+          index = file3.split("_")[-1].split(".")[0]
+          group_id = index // 1000
+
+          os.system(f"mv {file3} {group_id}/{file_3}")
+    
 
 
+      #let user know that residue is done
+      print(f"{dir1} DONE")
 
+    os.chdir("..")
 
-
-
-      
-      os.chdir("..")
+  #when entire system is done, let user know
+  print("all residues in system done")
 
 
 
