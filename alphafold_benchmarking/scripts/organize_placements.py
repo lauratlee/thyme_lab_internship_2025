7d8eb8c3eef64ea76e600325fbda02b4950c3a69
Laura Lee <lauratlee48@gmail.com>
2025-07-31 17:03:24 -0400

    Create organize_placements.py

---
+++ alphafold_benchmarking/scripts/organize_placements.py
@@ -0,0 +1,31 @@
+#organizes residue folder placements into groups of 1000 by making folders named in multiples (0, 1, 2, ...n(x1000)) and moving files into these new folders
+#run from a system directory, e.g. system_dir_h_bonds/
+
+import os
+
+for dir in os.listdir(os.getcwd()):
+  #filter for residue placement folders
+  if "res_" in dir:
+    os.chdir(dir)
+
+    #iterate thru files in residue folder to count total number of placements
+    count = 0
+    for r,d,f in os.walk(os.getcwd()):
+      for file in f:
+        if ".pdb" in file:
+          count += 1
+
+    print(f"{count} placements in {dir}")
+  os.chdir("..")
+
+
+
+
+
+
+
+
+
+
+
+
