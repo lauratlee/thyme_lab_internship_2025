061570e2cbb0843f411f47787a24fc72017ed9f6
Laura Lee <lauratlee48@gmail.com>
2025-08-04 11:24:38 -0400

    Update get_placement_rmsd.py

--- alphafold_benchmarking/scripts/get_placement_rmsd.py
+++ alphafold_benchmarking/scripts/get_placement_rmsd.py
@@ -89,8 +89,8 @@ with pymol2.PyMOL() as pymol:
 			if num_ref_atoms == 0:
 				print("WARNING: no atoms in reference. Exiting.")
 				sys.exit(1)
-			else:
-				print(f"ATOMS IN REFERENCE: {num_ref_atoms}")
+			#else:
+				#print(f"ATOMS IN REFERENCE: {num_ref_atoms}")
 
 
 			#create a dictionary the holds the placement files (and the residue they were derived from) and the corresponding rmsd values
@@ -136,8 +136,8 @@ with pymol2.PyMOL() as pymol:
 							if num_pla_atoms == 0:
 								print("WARNING: no atoms in placement. Exiting.")
 								sys.exit(1)
-							else:
-								print(f"ATOMS IN PLACEMENT: {num_pla_atoms}")
+							#else:
+								#print(f"ATOMS IN PLACEMENT: {num_pla_atoms}")
 
 							#align placement to reference. if alignment fails then skip file
 							try:
