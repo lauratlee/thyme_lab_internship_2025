089a2801a4101c7c7285dd7a29dc85cbdd7198b1
Laura Lee <lauratlee48@gmail.com>
2025-08-04 14:33:51 -0400

    Update run_rmsd_jobs.py

--- alphafold_benchmarking/scripts/run_rmsd_jobs.py
+++ alphafold_benchmarking/scripts/run_rmsd_jobs.py
@@ -3,6 +3,7 @@
 import os
 
 for file in os.listdir(os.getcwd()):
-  if "rmsd.slurm" in file or "rmsd_close_res.slurm" in file:
+  #if "rmsd.slurm" in file or "rmsd_close_res.slurm" in file:
+  if "ddg_rmsd.slurm" in file or "ddg_rmsd_close_res.slurm" in file:
     os.system(f"bsub {file}")
 
