5c1629783b6097048768fc5741d7c5a89d828b44
Laura Lee <lauratlee48@gmail.com>
2025-08-04 14:44:42 -0400

    Update get_placement_rmsd_by_ddg.py

--- alphafold_benchmarking/scripts/get_placement_rmsd_by_ddg.py
+++ alphafold_benchmarking/scripts/get_placement_rmsd_by_ddg.py
@@ -262,6 +262,7 @@ with pymol2.PyMOL() as pymol:
 
 			#sort placements_list by rmsd in ascending order
 			sorted_list = sorted(placements_list, key=lambda x: x[3])
+			print(sorted_list)
 
 			#get the best (lowest) rmsd entry
 			best_rmsd_ddg_10_entry = sorted_list[0]
