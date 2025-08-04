db1189b9b0ab4e39aa47516b31d401aa48b99832
Laura Lee <lauratlee48@gmail.com>
2025-08-04 11:46:44 -0400

    Update get_placement_rmsd.py

--- alphafold_benchmarking/scripts/get_placement_rmsd.py
+++ alphafold_benchmarking/scripts/get_placement_rmsd.py
@@ -225,7 +225,7 @@ with pymol2.PyMOL() as pymol:
 
 			#write result to system file
 			with open(f"{system}_placements_summary.csv", "w") as system_file:
-				system_file.write(f"{best_rmsd_1[0]},{best_rmsd_1[1]},{best_rmsd_1[2]:.3f}\n")
+				system_file.write(f"{best_rmsd_1[0]},{best_rmsd_1[1]},{best_rmsd_1[2]}\n")
 
 			#exit system directory to write data to general best rmsds file
 			os.chdir("..")
