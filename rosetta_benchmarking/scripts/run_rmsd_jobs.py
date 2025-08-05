9938563b02bce62f4fb731c2988f0e608bc37847
Laura Lee <lauratlee48@gmail.com>
2025-08-05 15:39:44 -0400

    Update run_rmsd_jobs.py

--- rosetta_benchmarking/scripts/run_rmsd_jobs.py
+++ rosetta_benchmarking/scripts/run_rmsd_jobs.py
@@ -3,7 +3,6 @@
 import os
 
 for file in os.listdir(os.getcwd()):
-  if "rmsd.slurm" in file or "rmsd_close_res.slurm" in file:
-  #if "ddg_rmsd.slurm" in file or "ddg_rmsd_close_res.slurm" in file:
+  if "_rmsd.slurm" in file or "_rmsd_close_res.slurm" in file or "ddg_rmsd.slurm" in file or "ddg_rmsd_close_res.slurm" in file:
     os.system(f"bsub {file}")
 
