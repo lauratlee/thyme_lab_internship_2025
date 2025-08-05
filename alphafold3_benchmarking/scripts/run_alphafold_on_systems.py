37e74101a19f528e35518e885f2cac9e064a3fe0
Laura Lee <lauratlee48@gmail.com>
2025-08-05 16:28:14 -0400

    Update run_alphafold_on_systems.py

--- alphafold3_benchmarking/scripts/run_alphafold_on_systems.py
+++ alphafold3_benchmarking/scripts/run_alphafold_on_systems.py
@@ -46,11 +46,11 @@ for r,d,f in os.walk(this_script_path + "/../system_dir/"):
 			os.system("mkdir " + this_script_path + "/../alphafold3/" + dire)
 
 			#make folders called af_input and af_output for inputs and outputs
-			os.system("mkdir " + this_script_path + "/../alphafold3/" + dire + "/af_input")
-			os.system("mkdir " + this_script_path + "/../alphafold3/" + dire + "/af_output")
+			os.system("mkdir " + this_script_path + "/../alphafold3_benchmarking/" + dire + "/af_input")
+			os.system("mkdir " + this_script_path + "/../alphafold3_benchmarking/" + dire + "/af_output")
 
 			#now, create the json protein-only file to run alphafold and needs to be run first
-			json_file = open(this_script_path + "/../alphafold3/" + dire + "/af_input/" + dire + "_protein_only.json", "w")
+			json_file = open(this_script_path + "/../alphafold3_benchmarking/" + dire + "/af_input/" + dire + "_protein_only.json", "w")
 
 			#open the smiles and residue sequence files to get that dat afor the json file as well
 			smiles_file = open(ligand_smile_location, "r")
@@ -121,7 +121,7 @@ for r,d,f in os.walk(this_script_path + "/../system_dir/"):
 			json_file.close()
 
 			#now write the corresponding shell script
-			shell_file = open(this_script_path + "/../alphafold3/" + dire + "/" + dire + "_no_inference.sh", "w")
+			shell_file = open(this_script_path + "/../alphafold3_benchmarking/" + dire + "/" + dire + "_no_inference.sh", "w")
 
 			#for now at least, I'm just going to hard code the path variables, since I'm not sure if this will really be reused
 
@@ -129,8 +129,8 @@ for r,d,f in os.walk(this_script_path + "/../system_dir/"):
 			shell_file.write("export AF3_RESOURCES_DIR=/pi/summer.thyme-umw/alphafold3\n")
 			shell_file.write("export AF3_IMAGE=${AF3_RESOURCES_DIR}/alphafold3_cuda7.sif\n")
 			shell_file.write("export AF3_CODE_DIR=${AF3_RESOURCES_DIR}/code\n")
-			shell_file.write("export AF3_INPUT_DIR=/pi/summer.thyme-umw/2024_intern_lab_space/laura_work/thyme_lab_internship_2025/alphafold3/" + dire + "/af_input\n")
-			shell_file.write("export AF3_OUTPUT_DIR=/pi/summer.thyme-umw/2024_intern_lab_space/laura_work/thyme_lab_internship_2025/alphafold3/" + dire + "/af_output\n")
+			shell_file.write("export AF3_INPUT_DIR=/pi/summer.thyme-umw/2024_intern_lab_space/laura_work/thyme_lab_internship_2025/alphafold3_benchmarking/" + dire + "/af_input\n")
+			shell_file.write("export AF3_OUTPUT_DIR=/pi/summer.thyme-umw/2024_intern_lab_space/laura_work/thyme_lab_internship_2025/alphafold3_benchmarking/" + dire + "/af_output\n")
 			shell_file.write("export AF3_MODEL_PARAMETERS_DIR=${AF3_RESOURCES_DIR}/params\n")
 			shell_file.write("export AF3_DATABASES_DIR=${AF3_RESOURCES_DIR}/db\n")
 			shell_file.write("\n")
@@ -159,7 +159,7 @@ for r,d,f in os.walk(this_script_path + "/../system_dir/"):
 			shell_file.close()
 
 			#chmod the shell file so it can be executed
-			os.system("chmod 777 " + this_script_path + "/../alphafold3/" + dire + "/" + dire + "_no_inference.sh")
+			os.system("chmod 777 " + this_script_path + "/../alphafold3_benchmarking/" + dire + "/" + dire + "_no_inference.sh")
 
 			#run a bsub job with the shell file
-			os.system("bsub -n 8 -R \"rusage[mem=2048]\" -W 300 -gpu \"num=1:gmodel=TeslaV100_SXM2_32GB-30G:mode=shared:j_exclusive=no\" -q gpu  -o " + this_script_path + "/../alphafold3/" + dire + "/" + dire + "_protein_preparation_log.txt bash " + this_script_path + "/../alphafold3/" + dire + "/" + dire + "_no_inference.sh")
+			os.system("bsub -n 8 -R \"rusage[mem=2048]\" -W 300 -gpu \"num=1:gmodel=TeslaV100_SXM2_32GB-30G:mode=shared:j_exclusive=no\" -q gpu  -o " + this_script_path + "/../alphafold3_benchmarking/" + dire + "/" + dire + "_protein_preparation_log.txt bash " + this_script_path + "/../alphafold3_benchmarking/" + dire + "/" + dire + "_no_inference.sh")
