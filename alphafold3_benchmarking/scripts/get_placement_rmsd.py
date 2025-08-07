658bd25923dcc01c437eb880bb14c8e6c9fcd603
Laura Lee <lauratlee48@gmail.com>
2025-08-07 13:49:13 -0400

    Update get_placement_rmsd.py

--- alphafold3_benchmarking/scripts/get_placement_rmsd.py
+++ alphafold3_benchmarking/scripts/get_placement_rmsd.py
@@ -77,7 +77,7 @@ with pymol2.PyMOL() as pymol:
 
 			#now, iterate over the placements
 			print(this_script_path + f"/../../alphafold3_benchmarking/{dire}")
-			for r2,d2,f2 in os.walk(this_script_path + f"/../../alphafold3_benchmarking/{dire}"):
+			for r2,d2,f2 in os.walk(f"/../../alphafold3_benchmarking/{dire}"):
 				for file in f2:
 					#if it is the confidence file
 					if file == (dire.lower() + "_ranking_scores.csv"):
