4b1265a7faf4eb903f6dcdd2fea168f71c1738cd
Ginsparg <abgvg9@uab.edu>
2025-11-24 18:22:24 -0500

    pushing potentially fixed code with laura method for rms

---
+++ rosetta_benchmarking/scripts/ari_get_rmsds_rosetta_laura_method.py
@@ -0,0 +1,229 @@
+#the purpose of this script is to go system by system and determine the best RMSDS for the best, top 10, and all placements per system using the 250 conformer schema
+#this script is to be run in the scripts directory like: python ari_get_rmsds_rosetta.py
+
+#if you only want to look at a single system or group of systems, add them after the call
+
+#import
+import os,sys
+import pymol2
+from rdkit import Chem
+from rdkit.Chem import AllChem, rdMolAlign
+#import numpy as np
+#from openbabel import openbabel as ob
+from openbabel import pybel
+#import re
+from rdkit.Chem import SanitizeFlags
+from rdkit.Chem import AllChem
+from pymol import CmdException
+
+#helper function to get ddg value from a placement .pdb file
+def get_ddg(file):
+  #open pdb and parse
+  with open(file, 'r') as placement:
+    for line in placement:
+      if "Scoring: Post-HighResDock system ddG:" in line:
+        parts = line.strip().split()
+        ddg_value = float(parts[-1])
+        break
+  
+  return ddg_value
+
+
+#helper function to strip all bond orders by setting everything to single bonds
+def strip_bond_orders(mol):
+	rw_mol = Chem.RWMol(mol)
+	for bond in rw_mol.GetBonds():
+		bond.SetBondType(Chem.rdchem.BondType.SINGLE)
+	Chem.SanitizeMol(rw_mol)
+	return rw_mol.GetMol()
+
+
+#collect select systems if there are any
+select_systems = []
+
+if len(sys.argv) > 1:
+	#add each specific system
+	for i in range(len(sys.argv)):
+		if i > 0:
+			select_systems.append(sys.argv[i])
+
+#print the select systems for confirmation
+print("Selected systems:")
+print(select_systems)
+
+#create a dictionary that holds the systems and a list for the best rmsds per system
+systems_rmsds = {}
+
+#store the workign location, which should be thyme_lab_internship_2025/rosetta_benchmarking/scripts/
+starting_script_dir = os.getcwd()
+
+print("starting script directory, which should contain thyme_lab_internship_2025/rosetta_benchmarking/scripts", starting_script_dir)
+
+#move to the system directory
+os.chdir("../system_dir/")
+
+#store the system directory
+system_dir = os.getcwd()
+
+#make a file to write the results
+write_file = open("all_ari_rosetta_placement_results.txt", "w")
+
+#iterate over the systems directories and go system by system, collecting the best rmsds
+for r,d,f in os.walk(system_dir):
+	#go over each directory
+	for dire in d:
+		#ensure that the root is the system directory
+		if r == system_dir:
+			#ensure the directory is in the select systems and we are selecting systems
+			if dire not in select_systems and len(select_systems) > 0:
+				print(dire + " not in selected systems.")
+				continue
+
+			print("On system: ", dire)
+
+			#create a list that holds all placements with the rmsd, ddg, and system file
+			#this will get sorted at the end by ddg to derive the top ddg for the best of all, 10, and single lowest ddg
+			placements_data = []
+
+			#we have our system, now go through and get the original ligand placement data
+			#store native ligand placement data as a dictionary
+			native_ligand_coords = {}
+
+			#read the native ligand file
+			native_lig_file = open(r + "/" + dire + "/ligand.mol2","r")
+			#note when we are in the atom block
+			in_atom_block = False
+			for line in native_lig_file.readlines():
+				#determine if we are starting the atom block
+				if line.startswith("@<TRIPOS>ATOM"):
+					in_atom_block = True
+					#continue since this line has no data
+					continue
+
+				#if we hit the bond block, we are done collecting atoms and can end
+				if line.startswith("@<TRIPOS>BOND"):
+					in_atom_block = False
+
+				#if we are in the atom block, get the atom data for the line
+				if in_atom_block:
+					atom_name = line.split()[1]
+					atom_x = float(line.split()[2])
+					atom_y = float(line.split()[3])
+					atom_z = float(line.split()[4])
+
+					#skip hydrogens
+					if atom_name.startswith("H"):
+						continue
+
+					native_ligand_coords[atom_name] = [atom_x,atom_y,atom_z]
+
+			#test print of the native atom data
+			print("Native atom data: ", native_ligand_coords)
+
+			#read reference ligand into rdkit without hydrogens
+			ref_ligand = Chem.MolFromMolFile(r + "/" + dire + "/ligand.mol2", removeHs=True)
+
+			#strip bond orders from reference
+			ref_ligand = strip_bond_orders(ref_ligand)
+
+			#check that reference loaded successfully
+			if ref_ligand is None:
+				print("WARNING: reference ligand did not read into rdkit. Exiting.")
+				sys.exit(1)
+				
+			#get smiles of reference ligand
+			ref_smiles = Chem.MolToSmiles(ref_ligand)			
+
+			#now, read through each system and derive the placement
+			for r2,d2,f2 in os.walk(r + "/" + dire):
+				for placement_file in f2:
+					if "_individual_conf_" in placement_file and placement_file.endswith(".pdb"):
+						#we have a placement file; derive the ligand data
+						placement_ligand_coords = {}
+
+						#variable to store teh ddg
+						ddg = 100
+
+						placement_read_file = open(r2 + "/" + placement_file, "r")
+						for line in placement_read_file.readlines():
+							if line.startswith("Scoring: Post-HighResDock system ddG:"):
+								ddg = float(line.strip().split()[4])
+
+
+
+						#derive the rmsd
+						rmsd = 100
+
+						#create a ligand file
+						os.system("grep \"^HETATM\" " + r2 + "/" + placement_file + " | grep -v -E \" ZN | MG | NA | K | CA | FE | MN | CU | CO | NI | CD | HG | SR | YB \" > " + r2 + "/ligand_only.pdb")
+
+						#read placement ligand into rdkit without hydrogens
+						pla_ligand = Chem.MolFromMolFile(r2 + "/ligand_only.pdb", removeHs=True)
+
+						#strip bond orders from placement
+						pla_ligand = strip_bond_orders(pla_ligand)
+
+						#use the get best RMS function to derive the rmsd
+						if ref_ligand and pla_ligand:
+							try:
+								rmsd = rdMolAlign.CalcRMS(ref_ligand, pla_ligand)
+								print(placement_file, rmsd)
+							except RuntimeError as e:
+								print("Alignment failed:", e)
+
+						#send values to the list
+						placements_data.append([rmsd,ddg,r2 + "/" + placement_file])
+
+			if len(placements_data) == 0:
+				print("NO PLACEMENTS FOR THIS SYSTEM!")
+				continue
+
+			#done looking at placements, derive the best rmsd for the ddg cutoffs
+			#sort the placements_data by ddg
+			placements_data = sorted(placements_data, key=lambda x: x[1])
+
+			#test print of top 10 ddg
+			#for i in range(0,10):
+			#	print(placements_data[i])
+
+			#determine the best rmsd for all, top 10, and top 1
+			top_1_rmsd = placements_data[0]
+
+			#top 10
+			#default to keeping the first
+			top_10_rmsd = placements_data[0]
+			for i in range(0,10):
+				#safety check to make sure we don't run out of bounds in case there are fewer than 10 palcements (which seems unlikely)
+				if len(placements_data) > i:
+					#check if better than currently held
+					if placements_data[i][0] < top_10_rmsd[0]:
+						top_10_rmsd = placements_data[i]
+
+
+			#top all
+			top_all_rmsd = placements_data[0]
+			for i in range(0,len(placements_data)):
+
+				#check if better than currently held
+				if placements_data[i][0] < top_all_rmsd[0]:
+					top_all_rmsd = placements_data[i]
+
+			#return the top rmsds
+			print("top all: ", top_all_rmsd)
+			print("top 10: ", top_10_rmsd)
+			print("top 1: ", top_1_rmsd)
+
+			write_file.write(dire + "\n")
+			write_file.write(f"top all: {top_all_rmsd}\n")
+			write_file.write(f"top 10: {top_10_rmsd}\n")
+			write_file.write(f"top 1: {top_1_rmsd}\n")
+
+			#open and write a system_specific file to write the data for too
+			system_write_file = open(r + "/" + dire + "/" + dire + "_best_rmsds.txt","w")
+			system_write_file.write(dire + "\n")
+			system_write_file.write(f"top all: {top_all_rmsd}\n")
+			system_write_file.write(f"top 10: {top_10_rmsd}\n")
+			system_write_file.write(f"top 1: {top_1_rmsd}\n")
+
+			#return to the system directory
+			os.chdir(system_dir)
