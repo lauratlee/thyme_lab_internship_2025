a0dffe9764dc94229d0eb2a36fd3bcb22823bc1f
Ginsparg <abgvg9@uab.edu>
2025-11-24 18:29:34 -0500

    ficing debug statement

--- rosetta_benchmarking/scripts/ari_get_rmsds_rosetta_laura_method.py
+++ rosetta_benchmarking/scripts/ari_get_rmsds_rosetta_laura_method.py
@@ -165,7 +165,7 @@ for r,d,f in os.walk(system_dir):
 						#strip bond orders from placement
 						pla_ligand = strip_bond_orders(pla_ligand)
 
-						print(ref_ligand,pla_ligand)
+						print(Chem.MolToSmiles(ref_ligand),Chem.MolToSmiles(pla_ligand))
 
 						#use the get best RMS function to derive the rmsd
 						if ref_ligand and pla_ligand:
